aaa5d8ab602b6254c745dad53a1a16d3fc5ff3f1
Nikki .m <nikkihappy007@gmail.com>
2025-10-04 17:10:12 +0000

    update all the project font to - Segoe UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        headline: ['"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
