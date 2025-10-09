dbb1c2dda80eb71956b75e3ae99c4767c57fd6d7
MALLIPUDI MANOHAR <202117bh174@wilp.bits-pilani.ac.in>
2025-10-09 20:10:14 +0000

    when i open the webapp in any browser, i see th efirebase icon . From wh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
       <body className="font-body antialiased">
         <FirebaseProvider>
