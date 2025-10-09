7f245e55e87f9c81e1d2ec35b6bcf9f0b8d2a812
MALLIPUDI MANOHAR <202117bh174@wilp.bits-pilani.ac.in>
2025-10-09 18:25:39 +0000

    It is still KO. Sa you can see in the attached image, the whole main pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <header className="px-4 lg:px-6 h-16 flex items-center border-b">
-        <div className="container flex items-center gap-4">
+        <div className="container flex items-center justify-between mx-auto">
             <Link href="#" className="flex items-center justify-center gap-2" prefetch={false}>
             <Logo className="h-6 w-6 text-primary" />
             <span className="text-lg font-bold">EaseSpace</span>
